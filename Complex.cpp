46290a3418057c9759821ee457134a94cdf432b2
jacob-a2k <jakub.a2k@gmail.com>
2023-03-27 23:59:28 +0200

    Add project files.

---
+++ Complex.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class Complex {
+	int real;
+	int imagine;
+public:
+	Complex(int x, int y) :real(x), imagine(y){}
+};
+
+int main()
+{
+	Complex one(2, 17);
+
+	return 0;
+}
