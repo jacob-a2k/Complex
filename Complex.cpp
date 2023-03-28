51bbd694260bd8007bc7e91bd730f6f2350ca7f9
jacob-a2k <jakub.a2k@gmail.com>
2023-03-28 08:31:08 +0200

    create CopyConstructor

--- Complex.cpp
+++ Complex.cpp
@@ -6,12 +6,18 @@ class Complex {
 	int real;
 	int imagine;
 public:
-	Complex(int x, int y) :real(x), imagine(y) {}
+	Complex(int r, int i) :real(r), imagine(i) {}
+	Complex(Complex& source);
 };
+Complex::Complex(Complex& source) {
+	real = source.real;
+	imagine = source.imagine;
+}
 
 int main()
 {
 	Complex one(2, 17);
+	Complex two(one);
 
 	return 0;
 }
