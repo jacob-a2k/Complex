d78e7bf4851cc69ba592e571be827678c63768fd
jacob-a2k <jakub.a2k@gmail.com>
2023-03-28 12:20:18 +0200

    create add();subtract();

--- Complex.cpp
+++ Complex.cpp
@@ -7,17 +7,38 @@ class Complex {
 	int imagine;
 public:
 	Complex(int r, int i) :real(r), imagine(i) {}
+	Complex();
 	Complex(const Complex& source);
+	Complex add(const Complex& source) const;
+	Complex subtract(const Complex& source) const;
+	//void operator=(const Complex& source);
+	void display()const;
 };
 Complex::Complex(const Complex& source) {
-	real = source.real;
-	imagine = source.imagine;
+	this->real = source.real;
+	this->imagine = source.imagine;
+}
+Complex Complex::add(const Complex& source)const {
+	return Complex(this->real + source.real, this->imagine + source.imagine);
+}
+Complex Complex::subtract(const Complex& source) const {
+	return Complex(this->real - source.real, this->imagine - source.imagine);
+}/*
+void Complex::operator=(const Complex& source) {
+	this->real = source.real;
+	this->imagine = source.imagine;
+}*/
+void Complex::display() const {		// for testing
+	cout << real << "," << imagine <<"i" << endl;
 }
 
 int main()
 {
 	Complex one(2, 17);
 	Complex two(one);
+	Complex result = one.add(two);
+	result = one.subtract(two);
+	result.display();
 
 	return 0;
 }
