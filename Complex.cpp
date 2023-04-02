e3e1375251289079e701405ca93ed32177c0602e
jacob-a2k <jakub.a2k@gmail.com>
2023-04-02 12:13:49 +0200

    change constructor to default constructor; overload assignment operator

--- Complex.cpp
+++ Complex.cpp
@@ -7,14 +7,13 @@ class Complex {
 	float real;
 	float imagine;
 public:
-	Complex(float r, float i) :real(r), imagine(i) {}
-	Complex();
+	Complex(float r = 0, float i = 0) :real(r), imagine(i) {}
 	Complex(const Complex& source);
 	Complex add(const Complex& source) const;
 	Complex subtract(const Complex& source) const;
 	Complex multiply(const Complex& source) const;
 	Complex divide(const Complex& source)const ;
-	void operator=(const Complex& source);
+	Complex& operator=(const Complex& source);
 	void display()const;
 };
 Complex::Complex(const Complex& source) {
@@ -34,9 +33,11 @@ Complex Complex::divide(const Complex& source)const  {
 	float divisor = pow(source.real,2) + pow(source.imagine,2);
 	return Complex((this->real * source.real + this->imagine * source.imagine)/divisor, (this->real * source.imagine * (-1) + this->imagine * source.real)/divisor);
 }
-void Complex::operator=(const Complex& source) {
+Complex& Complex::operator=(const Complex& source) {
+	cout << "Overload execute" << endl; // just to test it works
 	this->real = source.real;
 	this->imagine = source.imagine;
+	return *this;
 }
 void Complex::display() const {
 	cout << this->real << "," << this->imagine <<"i" << endl;
@@ -47,10 +48,11 @@ int main()
 	Complex one(6, -2);
 	Complex four(3, 4);
 	Complex two(one);
-	Complex result = one.multiply(four);
+	Complex result; // = one.multiply(four);
 	//result = one.subtract(two);
 	//result = one.multiply(four);
 	//result = one.divide(four);
+	result = four;
 	result.display();
 
 	return 0;
